b03c828f22dff6e377ac16b657ce6f54dbcf94fd
Duevano Fairuz Pandya <114162555+duevanofairuz@users.noreply.github.com>
2023-05-16 21:58:10 +0700

    hapus newline

--- huffman.cpp
+++ huffman.cpp
@@ -1,18 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 struct MinHeapNode {
- 
     char data;
-
     unsigned freq;
-
     MinHeapNode *left, *right;
- 
     MinHeapNode(char data, unsigned freq)
- 
     {
- 
         left = right = NULL;
         this->data = data;
         this->freq = freq;
@@ -20,9 +14,7 @@ struct MinHeapNode {
 };
 
 struct compare {
- 
     bool operator()(MinHeapNode* l, MinHeapNode* r)
- 
     {
         return (l->freq > r->freq);
     }
@@ -30,13 +22,10 @@ struct compare {
  
 void printCodes(struct MinHeapNode* root, string str)
 {
- 
     if (!root)
         return;
- 
     if (root->data != '$')
         cout << root->data << ": " << str << "\n";
- 
     printCodes(root->left, str + "0");
     printCodes(root->right, str + "1");
 }
@@ -44,40 +33,31 @@ void printCodes(struct MinHeapNode* root, string str)
 void HuffmanCodes(char data[], int freq[], int size)
 {
     struct MinHeapNode *left, *right, *top;
- 
-    priority_queue<MinHeapNode*, vector<MinHeapNode*>,
-                   compare>
-        minHeap;
- 
+    priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+
     for (int i = 0; i < size; ++i)
         minHeap.push(new MinHeapNode(data[i], freq[i]));
- 
+
     while (minHeap.size() != 1) {
- 
         left = minHeap.top();
         minHeap.pop();
- 
+
         right = minHeap.top();
         minHeap.pop();
 
-        top = new MinHeapNode('$',
-                              left->freq + right->freq);
+        top = new MinHeapNode('$', left->freq + right->freq);
  
         top->left = left;
         top->right = right;
- 
         minHeap.push(top);
     }
- 
     printCodes(minHeap.top(), "");
 }
  
 int main()
 {
- 
     char arr[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
     int freq[] = { 5, 9, 12, 13, 16, 45 };
- 
     int size = sizeof(arr) / sizeof(arr[0]);
  
     HuffmanCodes(arr, freq, size);
